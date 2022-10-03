6336c1a22d9561a5365b188d55ee54d12e071c8a
Chiaki-Suzuki <c.suzuki.eba@gmail.com>
2022-10-03 16:37:28 +0900

    VueCLI 複数ページ対応（とりあえずトップと料金のみ）

---
+++ 12vuecli/src/pages/price/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import App from './Price.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(Price),
+}).$mount('#app')
--- 12vuecli/vue.config.js
+++ 12vuecli/vue.config.js
@@ -2,3 +2,18 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+// 複数ページ対応
+module.exports = {
+  pages: {
+    top: {
+      entry: 'src/pages/top/main.js', // エントリーポイントとなるjs
+      template: 'public/index.html', // テンプレートのHTML
+      filename: 'index.html', // build時に出力されるファイル名
+    },
+    price: {
+      entry: 'src/pages/price/main.js',
+      template: 'public/price.html',
+      filename: 'price.html',
+    },
+  },
+};
