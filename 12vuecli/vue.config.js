97c5b51f2db78b8616a0e2c735ab771a2c69911a
Chiaki-Suzuki <c.suzuki.eba@gmail.com>
2022-10-04 14:15:28 +0900

    ビルド

--- 12vuecli/vue.config.js
+++ 12vuecli/vue.config.js
@@ -6,34 +6,44 @@ module.exports = defineConfig({
 module.exports = {
   pages: {
     top: {
-      entry: 'src/pages/top/main.js', // エントリーポイントとなるjs
+      entry: './src/pages/top/main.js', // エントリーポイントとなるjs
       template: 'public/index.html', // テンプレートのHTML
       filename: 'index.html', // build時に出力されるファイル名
+      publicPath: './'
     },
     spa: {
-      entry: 'src/pages/spa/main.js',
+      entry: './src/pages/spa/main.js',
       template: 'public/spa.html',
       filename: 'spa.html',
+      publicPath: './'
     },
     room: {
-      entry: 'src/pages/room/main.js',
+      entry: './src/pages/room/main.js',
       template: 'public/room.html',
       filename: 'room.html',
+      publicPath: './'
     },
     dining: {
-      entry: 'src/pages/dining/main.js',
+      entry: './src/pages/dining/main.js',
       template: 'public/dining.html',
       filename: 'dining.html',
+      publicPath: './'
     },
     price: {
-      entry: 'src/pages/price/main.js',
+      entry: './src/pages/price/main.js',
       template: 'public/price.html',
       filename: 'price.html',
+      publicPath: './'
     },
     access: {
-      entry: 'src/pages/access/main.js',
+      entry: './src/pages/access/main.js',
       template: 'public/access.html',
       filename: 'access.html',
+      publicPath: './'
     }
   },
 };
+// ビルド
+// module.exports = {
+//   publicPath: './'
+// }
